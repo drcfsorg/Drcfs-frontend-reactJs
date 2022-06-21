0a77f74f77d27ed870c0c4f9a6ed8b2b42823674
Prabesh <prabesh.bista369@gmail.com>
2022-06-21 14:55:34 +0545

    website icon changed

--- src/Components/Body/community/comunityImages.js
+++ src/Components/Body/community/comunityImages.js
@@ -1,8 +1,8 @@
 import depauwLogo from "../../../Assets/depauw.png";
 import goldenGateLogo from "../../../Assets/golden.jpg";
-import howardLogo from "../../../Assets/howard.jpg";
+import howardLogo from "../../../Assets/howard_social.png";
 import microsoftLogo from "../../../Assets/microsoft101.png";
-import tuLogo from "../../../Assets/tu.png";
+import tuLogo from "../../../Assets/TU_logo.png";
 
 
 const communityLogos=[

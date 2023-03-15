bd329389d53e3b0fae3b9c53c11bd5a3104057f6
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2023-03-15 13:49:51 +0545

    Removed redirecting and added download option

--- src/Components/VerifyPage/verify.js
+++ src/Components/VerifyPage/verify.js
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
 import styles from "./verify.module.css";
+import axios from "axios";
 
 const Verify = () => {
   const [token, setToken] = useState("");
+  const [imageSrc, setImageSrc] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (token === "") return;
     else {
-      window.location.href = `https://verify.drcfs.org/verify/${token}`;
+      const url = `/verify/${token}`;
+      setIsLoading(true);
+      fetchData(url);
       setToken("");
     }
+  };
+
+  const fetchData = async (url) => {
+    try {
+      const response = await axios.get(url, { responseType: "arraybuffer" });
+      const base64 = btoa(
+        new Uint8Array(response.data).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ""
+        )
+      );
+      const imageSrc = `data:image/png;base64,${base64}`;
+      if (imageSrc.length === 110) {
+        setError("Sorry your certificate was not found on our server :(");
+        setImageSrc(null);
+        setIsLoading(false);
+        return;
+      }
+      setImageSrc(imageSrc);
+      setError(null);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.download = "my-image.png";
+    link.href = imageSrc;
+    link.click();
   };
 
   return (
@@ -25,8 +62,24 @@ const Verify = () => {
           onChange={(event) => setToken(event.target.value)}
           className={styles.inputField}
         />
-        <input type="submit" value="Submit" className={styles.button} />
+        <button type="submit" className={styles.button} disabled={isLoading}>
+          Verify
+        </button>
       </form>
+      {isLoading && <h2 style={{ color: "#fff" }}>Loading....</h2>}
+      {!isLoading && imageSrc && (
+        <div className={styles.certificate}>
+          <img
+            src={imageSrc}
+            alt="certificate"
+            className={styles.certificateImage}
+          />
+          <button onClick={handleDownload} className={styles.button}>
+            Download
+          </button>
+        </div>
+      )}
+      {!isLoading && error && <h2 className={styles.errorMessage}>{error}</h2>}
     </div>
   );
 };
